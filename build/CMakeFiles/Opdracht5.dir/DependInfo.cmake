def784f91a469f8be028de251190ea280cfea115
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-12-07 08:58:52 +0100

    Setup project

---
+++ build/CMakeFiles/Opdracht5.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/embed2/Opdracht-5-GTK/main.c" "/home/pi/embed2/Opdracht-5-GTK/build/CMakeFiles/Opdracht5.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/gtk-2.0"
+  "/usr/lib/arm-linux-gnueabihf/gtk-2.0/include"
+  "/usr/include/gio-unix-2.0"
+  "/usr/include/cairo"
+  "/usr/include/pango-1.0"
+  "/usr/include/atk-1.0"
+  "/usr/include/pixman-1"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/libmount"
+  "/usr/include/blkid"
+  "/usr/include/harfbuzz"
+  "/usr/include/fribidi"
+  "/usr/include/glib-2.0"
+  "/usr/lib/arm-linux-gnueabihf/glib-2.0/include"
+  "/usr/include/uuid"
+  "/usr/include/freetype2"
+  "/usr/include/libpng16"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Opdracht5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opdracht5.dir/main.c.o"
+  "Opdracht5"
+  "Opdracht5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Opdracht5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
